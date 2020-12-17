b9ff2352c4f7c20bae69d68ed5f0aa20d022ed30
doublem1nt <jyeewasabi@gmail.com>
2020-12-16 18:55:42 -0800

    Completed application

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# squadContainer
+## "squadContainer" :: Team Employee Summary CLI Generator
+
+## Description
+Project is designed to demonstrate a thorough understanding of Javascript, Node and various Node Modules by creating a Command Line Application. This application will receive input from user, retrieving employee information and designating common and unique details for each person. Multiple employees are able to be inputted until the User decides to stop adding. Upon completion of final employee information input, a "Team.html" file is dynamically created in "output" directory for the User to view in a browser. 
+
+![demo](./assets/images/demo.gif?raw=true)
+
+## Table of Contents
+  
+* [Installation](#installation)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Contact](#contact)
+* [License](#license)
+  
+## Installation
+Install Node Modules 
+(1) Jest (https://jestjs.io/docs/en/getting-started)
+(2) Fs (https://www.npmjs.com/package/file-system)
+(3) Path (https://www.npmjs.com/package/path)
+(4) Inquirer (https://www.npmjs.com/package/inquirer)
+
+## Contributing
+Contribution is always appreciated. 
+(1) Folk the Project 
+(2) Create your feature Branch (git Checkout -b Your Branch) 
+(3) Commit your Changes (Git commit -m "Any Change you made on this Branch") 
+(4) Push to the Branch (git push origin YourBranch) (5) Open a Pull Request
+
+## Tests
+Manual Testing Only
+
+## Contact
+Please contact Jonathan Yee for any additional information
+Github Username: doublem1nt
+Email: jyeewasabi@gmail.com
+
+## License
+Dist. under MIT License
+
+## Badge
+
+[MIT](https://img.shields.io/badge/License-MIT-yellow.svg) (https://opensource.org/licenses/MIT)
--- app.js
+++ app.js
@@ -11,12 +11,10 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 const Employee = require("./lib/Employee");
 
+// empty Array to push Employee objects into
 const employeeArray = [];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// array of questions for user
+// array of questions for user, common properties for all employees, lines 19 to 33
 const questions = [
     {
         type: "input",
@@ -33,6 +31,7 @@ const questions = [
         message: "Thank you, please provide the employee's ID Number: ",
         name: "id",
     },
+    // Type of Employee determines next questions asked, lines 35 - 43
     {
         type: "list",
         message: "Thank you, please provide the employee's Role: ",
@@ -42,24 +41,28 @@ const questions = [
             "Intern"],
         name: "role",
     },
+    // If Employee is "Manager", Lines 45-50 are asked
     {
         type: "number",
         message: "Thank you, please provide the manager's office number: ",
         name: "officeNumber",
         when: (userData) => userData.role === "Manager",
     },
+    // If Employee is "Engineer", Lines 52-57 are asked
     {
         type: "input",
         message: "Thank you, please provide the engineer's Github Username: ",
         name: "github",
         when: (userData) => userData.role === "Engineer",
     },
+    // If Employee is "Intern", Lines 59-64 are asked
     {
         type: "input",
         message: "Thank you, please provide the intern's School: ",
         name: "school",
         when: (userData) => userData.role === "Intern",
     },
+    // Last Question to determine if additional employee iterations are to be included 
     {
         type: "confirm",
         message: "Thank you for the input, would you like to set up another Employee?",
@@ -67,31 +70,36 @@ const questions = [
     }
 ];
 
-function init() {
+// Initialization Function definition to create new Employee objects
+function buildNewEmployee() {
     inquirer
         .prompt(questions)
         .then((userData) => {
-
-            // const typeRole = ;
-
+            // Depending on Employee type, different data is stored in subclass Employee objects
+            // All newly created subclass employee objects are pushed into EmployeeArray 
+            // All subclasses have same first three properties (1) name (2) ID & (3) email
             switch (userData.role){
+
+                // Managers have unique property of "Office Number"
                 case "Manager":
                     const newManager = new Manager(userData.name, userData.id, userData.email, userData.officeNumber)
-                    console.log(newManager)
+                    // console.log(newManager) // uncomment to see each new Manager object created, in console!
                     employeeArray.push(newManager);
                     checkForMore(userData.add_more);
                     return true;
 
+                // Engineers have unique property of "Github Username"
                 case "Engineer":
                     const newEngineer = new Engineer(userData.name, userData.id, userData.email, userData.github)
-                    console.log(newEngineer)
+                    // console.log(newEngineer) // uncomment to see each new Manager object created, in console!
                     employeeArray.push(newEngineer);
                     checkForMore(userData.add_more);
-                    return true;
+                    return true; 
 
+                // Managers have unique property of "Office Number"
                 case "Intern":
                     const newIntern = new Intern(userData.name, userData.id, userData.email, userData.school)
-                    console.log(newIntern)
+                    // console.log(newIntern) // uncomment to see each new Manager object created, in console!
                     employeeArray.push(newIntern);
                     checkForMore(userData.add_more);
                     return true;
@@ -102,37 +110,37 @@ function init() {
         })
 }
 
+// Function to determine if user wishes to continue adding new employees into Employee Array
+// If not, function will activate next function to generate HTML
 function checkForMore(more) {
     if (more){
-        init();
+        // re-runs build function to create another employee object
+        buildNewEmployee();
     } else {
-        let teamDiagram = render(employeeArray);
-        fs.writeFile("team.html")
-        console.log("\nThank you, please see HTML file for your team details\n");
+        // Ends new employee object creation and moves application over to Html  & file creation
+        htmlGenerator();
     }
 }
 
-init();
+// Function uses Employee Array to customize data in htmlRenderer.js file and then writes the file into Output directory.
+function htmlGenerator() {
 
+    // If output directory doesn't exist, following lines dynamically create it and then write "team.html" into the new location
+    if (!fs.existsSync(OUTPUT_DIR)){
+        fs.mkdirSync(OUTPUT_DIR);
+        const teamDiagram = render(employeeArray)
+        fs.writeFileSync(outputPath, teamDiagram)
+        console.log("\nThank you, please see HTML file for your team details\n");
+        return true;
 
+    // If output directory does exist, following lines will dynamically create "team.html" into folder
+    } else {
+        const teamDiagram = render(employeeArray)
+        fs.writeFileSync(outputPath, teamDiagram)
+        console.log("\nThank you, please see HTML file for your team details\n");
+        return true;
+    }
+}
 
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+// Calling Initialization function to begin application
+buildNewEmployee();
